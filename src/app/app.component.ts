01b442256acecbff425c2539e5595383be96d376
Andy Ba <xbax486@gmail.com>
2018-08-08 09:21:37 +1000

    Initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { AuthService } from 'shared/services/auth/auth.service';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { UserService } from 'shared/services/user/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnDestroy{
+  subscription: Subscription;
+
+  constructor(
+    private authService: AuthService,
+    private userService: UserService, 
+    private router: Router) {
+
+    this.subscription = this.authService.user$.subscribe(user => {
+      if(!user) return;
+
+      this.userService.saveUser(user);
+      this.redirectUser();
+    });
+  }
+  
+  private redirectUser() {
+    let returnUrl = localStorage.getItem('returnUrl');
+    if(!returnUrl) return;
+    localStorage.removeItem('returnUrl');
+    this.router.navigateByUrl(returnUrl);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
+/* Modules */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
+import { AngularFireModule } from "angularfire2";
+import { environment } from '../environments/environment';
 
+import { CoreModule } from './core/core.module';
+import { SharedModule } from 'shared/shared.module';
+import { AdminModule } from './admin/admin.module';
+import { ShoppingModule } from './shopping/shopping.module';
+
+/* Components */
 import { AppComponent } from './app.component';
+import { LoginComponent } from "./core/components/login/login.component";
+import { ProductsComponent } from "./shopping/components/products/products.component";
+
+/* Guards */
+import { AuthGuard } from "./shared/guards/auth/auth-guard.service";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    CoreModule,
+    AdminModule,
+    ShoppingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: ProductsComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: '**',
+        component: LoginComponent
+      },
+    ]),
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
