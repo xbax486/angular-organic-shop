40a17d832c52f07d63481ee88fd398ff820b0476
Andy Ba <xbax486@gmail.com>
2018-08-08 09:34:45 +1000

    Build core module.

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,30 @@
+/* Modules */
+import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
+import { SharedModule } from 'shared/shared.module';
+
+/* Components */
+import { BsNavbarComponent } from './components/bs-navbar/bs-navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    RouterModule.forChild([
+      {
+        path: 'login',
+        component: LoginComponent
+      }
+    ])
+  ],
+  declarations: [
+    BsNavbarComponent,
+    HomeComponent,
+    LoginComponent,
+  ],
+  exports: [
+    BsNavbarComponent
+  ]
+})
+export class CoreModule { }
